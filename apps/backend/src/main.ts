1196e1b9991a7c4195a2aa5317440c2f1ce80726
lildibbb <adib.asyraaf760@gmail.com>
2025-02-21 00:17:03 +0800

    add Swagger API Documentation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Swagger API Documentation')
+    .setDescription('This is the API documentation for the Swagger API')
+    .setVersion('1.0')
+    .addTag('Swagger')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
